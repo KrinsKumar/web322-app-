ba838cbca0a77e07017da6377474089e4485bb06
Krins <krinskumar@gmail.com>
2023-02-16 23:06:28 -0500

    complete

--- blog-service.js
+++ blog-service.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
-*  WEB322 – Assignment 02
+*  WEB322 – Assignment 03
 *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
 *  (including 3rd party web sites) or distributed to other students.
 * 
-*  Name: Krinskumar Vaghasia Student ID: 169722212 Date: 3rd Feb 2023
+*  Name: Krinskumar Vaghasia Student ID: 169722212 Date: 15th Feb 2023
 *
 *  Cyclic Web App URL: https://dark-ruby-rhinoceros-cap.cyclic.app
 *
--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Krinskumar Vaghasia Student ID: 169722212 Date: 15th Feb 2023
+*
+*  Cyclic Web App URL: https://dark-ruby-rhinoceros-cap.cyclic.app
+*
+*  GitHub Repository URL: https://github.com/KrinsKumar/web322-app-
+*
+********************************************************************************/ 
 const express = require("express");
 const path = require("path")
 const app = express();
